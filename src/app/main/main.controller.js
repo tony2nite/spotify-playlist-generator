d3a1bf7fbe86946339bb45dfacd00565592812f2
Anthony Hughes <tony2nite@gmail.com>
2015-02-10 11:37:36 +0000

    Frontend now in accordion. Style cleaning and tidying

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,6 +10,11 @@ angular.module('spotifyPlaylistGenerator')
     $scope.playlistplaceholder = 'Spotify';
     $scope.loggedin = false;
     $scope.queries = [];
+    $scope.alerts = [];
+    $scope.tracks = [];
+
+    $scope.status = [true, false, false];
+    $scope.oneAtATime = true;
 
     var MAX_DELAY = 150;
     var MIN_DELAY = 850;
@@ -17,11 +22,31 @@ angular.module('spotifyPlaylistGenerator')
     /**
      * Private Functions
      */
-    var randomBetween = function(min, max) {
+
+    function log (msg) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function randomBetween (min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
     };
 
-    var delayedRequest = function(delay, scope, promise, args) {
+    function spotifyLogin () {
+      var defer = $q.defer();
+      Spotify.login().then(function (data) {
+        console.log("login", data);
+        $scope.loggedin = true;
+      }).then(function (data){
+        Spotify.getCurrentUser().then(function (data) {
+          console.log("getCurrentUser", data);
+          $scope.user = data.id;
+          defer.resolve(data);
+        })
+      });
+      return defer.promise;
+    };
+
+    function delayedRequest (delay, scope, promise, args) {
       var defer = $q.defer();
 
       $timeout(function() {
@@ -29,99 +54,11 @@ angular.module('spotifyPlaylistGenerator')
       }, delay);
 
       return defer.promise;
-    }
-
-
-    /**
-     * Public
-     */
-
-    $scope.updatePlaylistNamePlaceholder = function() {
-      var name = "Spotify";
-      var search = $scope.searchartist.trim();
-      if (search.length > 0) {
-        name = search;
-      }
-      $scope.playlistplaceholder = name;
-    }
-
-    $scope.login = function () {
-      Spotify.login().then(function (data) {
-        console.log(data);
-        alert("You are now logged in");
-        $scope.loggedin = true;
-        // Spotify.getCurrentUser().then(function (data) {
-        //   console.log(data);
-        //   $scope.user = data.id;
-        // })
-      }).then(function (data){
-        Spotify.getCurrentUser().then(function (data) {
-          console.log(data);
-          $scope.user = data.id;
-        })
-      });
     };
 
-    $scope.getArtistTracks = function () {
-      console.log($scope.queries);
+    function createPlaylist () {
+      var defer = $q.defer();
 
-      var calls = [];
-      for (var i = $scope.queries.length - 1; i >= 0; i--) {
-        // console.log($scope.queries[i].selected);
-        if ($scope.queries[i].selected !== null) {
-          calls.push(delayedRequest(
-            i * randomBetween(MIN_DELAY, MAX_DELAY), 
-            Spotify, Spotify.getArtistTopTracks, [$scope.queries[i].selected, 'GB']
-            ));
-        }
-      };
-      $q.all(calls).then(function (data) {
-        console.log(data);
-        var tracks = [];
-        for (var i = data.length - 1; i >= 0; i--) {
-          tracks = tracks.concat(data[i].tracks);
-        };
-        console.log(tracks);
-        // $scope.dynamic = 0;
-        $scope.tracks = tracks;
-      });
-    };
-
-    $scope.searchArtist = function () {
-      if (!$scope.searchartist || $scope.searchartist.length == 0) {
-        return;
-      }
-      var terms = $scope.searchartist.trim().split('\n');
-      $scope.dynamic = 0;
-
-      var counter = 0;
-      var request = function (artist) {
-        var defer = $q.defer();
-        Spotify.search(artist, 'artist', {limit: $scope.searchlimit}).then(
-          function(data) {
-            // console.log(counter, $scope.max);
-            console.log(data);
-            counter++;
-            $scope.dynamic = Math.round(counter / terms.length * $scope.max);
-            var selected = data.artists.items.length > 0 ? data.artists.items[0].id : null;
-            defer.resolve({q: artist, selected: selected, results: data});
-          }
-        );
-        return defer.promise;
-      }
-
-      var calls = [];
-      for (var i=0; i < terms.length; i++ ) {
-        calls.push(delayedRequest(i * randomBetween(MIN_DELAY, MAX_DELAY), this, request, [terms[i]]));
-      }
-      $q.all(calls).then(function (data) {
-        console.log(data);
-        // $scope.dynamic = 0;
-        $scope.queries = data;
-      });
-    };
-
-    $scope.createPlaylist = function() {
       Spotify.createPlaylist($scope.user, { name: $scope.playlist, public: false})
       .then(
         function (data) {
@@ -149,9 +86,121 @@ angular.module('spotifyPlaylistGenerator')
             ids = [];
           }
           $q.all(calls).then(function (data) {
+            // Playlist created
             console.log(data);
+            defer.resolve(data);
           });
         }
       );
-    }
+      return defer.promise;
+    };
+
+    function getArtistTracks () {
+      console.log($scope.queries);
+
+      $scope.status = [false, false, true];
+
+      var defer = $q.defer();
+      var calls = [];
+
+      for (var i = $scope.queries.length - 1; i >= 0; i--) {
+        // console.log($scope.queries[i].selected);
+        if ($scope.queries[i].selected !== null) {
+          calls.push(delayedRequest(
+            i * randomBetween(MIN_DELAY, MAX_DELAY), 
+            Spotify, Spotify.getArtistTopTracks, [$scope.queries[i].selected, 'GB']
+            ));
+        }
+      };
+
+      $q.all(calls).then(function (data) {
+        console.log(data);
+        var tracks = [];
+        for (var i = data.length - 1; i >= 0; i--) {
+          tracks = tracks.concat(data[i].tracks);
+        };
+        console.log(tracks);
+        // $scope.dynamic = 0;
+        $scope.tracks = tracks;
+        defer.resolve();
+      });
+
+      return defer.promise;
+    };
+
+    function addAlert  (message) {
+      $scope.alerts.push({type: 'success', msg: message});
+    };
+
+
+    /**
+     * Public
+     */
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
+
+    $scope.updatePlaylistNamePlaceholder = function() {
+      var name = "Spotify";
+      var search = $scope.searchartist.trim();
+      if (search.length > 0) {
+        name = search;
+      }
+      $scope.playlistplaceholder = name;
+    };
+
+    $scope.processPlaylist = function(playlistName) {
+      $scope.playlist = playlistName;
+
+      getArtistTracks()
+        .then(spotifyLogin)
+        .then(createPlaylist)
+        .then(function(){
+          addAlert('Playlist Created');
+        });
+    };
+
+    $scope.searchArtist = function (artists) {
+      $scope.searchartist = artists;     // This doesn't seem Angular but ng-model isn't being updated
+      if (!$scope.searchartist || $scope.searchartist.length == 0) {
+        return;
+      }
+      var terms = $scope.searchartist.trim().split('\n');
+      $scope.dynamic = 0;
+      $scope.status = [false, true, false];
+
+      var counter = 0;
+      function request (artist) {
+        var defer = $q.defer();
+        Spotify.search(artist, 'artist', {limit: $scope.searchlimit}).then(
+          function(data) {
+            console.log("Spotify.search", data);
+            counter++;
+            defer.notify(data);
+            $scope.dynamic = Math.round(counter / terms.length * $scope.max);
+            var selected = data.artists.items.length > 0 ? data.artists.items[0].id : null;
+            defer.resolve({q: artist, selected: selected, results: data});
+          }
+        );
+        return defer.promise;
+      }
+
+
+
+      var calls = [];
+      for (var i=0; i < terms.length; i++ ) {
+        calls.push(delayedRequest(i * randomBetween(MIN_DELAY, MAX_DELAY), this, request, [terms[i]]));
+      }
+      $q.all(calls).then(
+        function (data) {
+        // console.log(data);
+        // $scope.dynamic = 0;
+        $scope.queries = data;
+      }, function (data) {
+        // Error
+      }, function(data){
+        // Notify
+        console.log("notify", data);
+      });
+    };
   }]);
